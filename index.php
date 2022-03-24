b2318ed5b2286be008fa149c637f38b02aeebcda
khshakilahamed <khshakil.ahamed18@gmail.com>
2022-03-25 01:15:24 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    if($_SERVER["REQUEST_METHOD"]=='POST'){
+        echo $_POST['name']."<br>"; 
+        echo $_POST['email']."<br>";
+        echo $_POST['gender']."<br>";
+        echo $_POST['gender2']."<br>";
+    }
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        Name: <input type="text" name="name" /> <br> <br>
+        Email: <input type="text" name="email" /> <br> <br>
+        <select name="gender" id="">
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="Female">Female</option>
+        </select>
+        <br> <br>
+        Gender: <br>
+        <input type="radio" name="gender2" value="male" id=""> Male <br>
+        <input type="radio" name="gender2" value="female"> Female <br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+    echo $_POST['name']."<br>"; 
+    echo $_POST['email']."<br>";
+    echo $_POST['gender']."<br>";
+    echo $_POST['gender2']."<br>";
+ ?>
