0a03d34afb68572c4a51597b93a338a433d584b8
khshakilahamed <khshakil.ahamed18@gmail.com>
2022-03-26 01:44:45 +0600

    include & require added

--- 2_date.php
+++ 2_date.php
@@ -1,4 +1,7 @@
 <?php 
-date_default_timezone_set("Asia/Dhaka");
-echo date("Y:M:D h-i-sa");
+    date_default_timezone_set("Asia/Dhaka");
+    echo date("Y:M:D h-i-sa")."<br>";
+    echo date("y:m:d h-i-sa")."<br>";
+    echo date("y/m/d h-i-sa")."<br>"; // formate changed
+    echo date("d/m/y h-i-sa")."<br>"; // formate changed
 ?>
---
+++ 2_include_require.php
@@ -0,0 +1,6 @@
+<?php 
+    echo "this is include & require file <br>";
+    include "2_date.php";
+    echo "<br> <br>";
+    require "2_date.php";
+?>
